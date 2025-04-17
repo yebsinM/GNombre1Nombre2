370194b4794770f683af9d9bde450cfd349a529f
FAMILIA <FAMILIA@192.168.1.221>
2025-04-17 11:04:54 +0200

    ultimo cambio

---
+++ src/pronombre1nombre2/Pronombre1nombre2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pronombre1nombre2;
+
+/**
+ *
+ * @author FAMILIA
+ */
+public class Pronombre1nombre2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Mi primer nombre es Yebsin");
+        System.out.println("Mi segundo nombre es Miguel");
+        System.out.println("Mi primer apellido Capcha");
+        System.out.println("Mi segundo apellido es Hilari");
+    }
+    
+}
