d335ac11577be24581293f360b3172e75ac447a6
FAMILIA <FAMILIA@192.168.1.221>
2025-04-18 17:08:11 +0200

    NUEVO COMMIT

--- src/nuevo_paquete/nueva_clase.java
+++ src/nuevo_paquete/nueva_clase.java
@@ -12,5 +12,11 @@ public class nueva_clase {
     public static void main(String args[]){
         
         System.out.println("Segunda clase intento dos");
+        System.out.println("PROBAMOS");
+        System.out.println("UN");
+        System.out.println("NUEVO");
+        System.out.println("CAMBIO");
+        
+        
     }
 }
